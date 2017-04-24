f8bb63508b839beda0fee6f1c5f3fdeaa81ecc72
yanil3500 <yanil3500@gmail.com>
2017-04-24 13:54:15 -0700

    Seeds Core Data DB with hotel data from son

--- core-data-hotel/core-data-hotel/AppDelegate.m
+++ core-data-hotel/core-data-hotel/AppDelegate.m
@@ -9,6 +9,12 @@
 #import "AppDelegate.h"
 #import "ViewController.h"
 
+#import "Hotel+CoreDataClass.h"
+#import "Hotel+CoreDataProperties.h"
+
+#import "Room+CoreDataClass.h"
+#import "Room+CoreDataProperties.h"
+
 @interface AppDelegate ()
 
 @property(strong, nonatomic) UINavigationController *navController;
@@ -22,7 +28,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     [self setupRootViewController];
-    
+    [self bootStrapApp];
     return YES;
 
 }
@@ -67,6 +73,61 @@
     [self saveContext];
 }
 
+-(void)bootStrapApp{
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Hotel"];
+    
+    NSError *error;
+    
+    NSInteger count = [self.persistentContainer.viewContext countForFetchRequest:request error:&error];
+    
+    if (error) {
+        NSLog(@"%@", error.localizedDescription);
+    }
+    
+    if (count == 0){
+        NSDictionary *hotels = [[NSDictionary alloc]init];
+        
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"hotels" ofType:@"json"];
+        
+        NSData *jsonData = [NSData dataWithContentsOfFile:path];
+        
+        NSError *jsonError;
+        
+        NSDictionary *jsonDictionary = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableContainers error:&jsonError];
+        
+        if (jsonError){
+            NSLog(@"%@", jsonError.localizedDescription);
+        }
+        
+        hotels = jsonDictionary[@"Hotels"];
+        
+        for (NSDictionary *hotel in hotels){
+            Hotel *newHotel = [NSEntityDescription insertNewObjectForEntityForName:@"Hotel"
+                                                            inManagedObjectContext:self.persistentContainer.viewContext];
+            newHotel.name = hotel[@"name"];
+            newHotel.location = hotel[@"location"];
+            newHotel.stars = (NSInteger)hotel[@"stars"];
+            for (NSDictionary *room in hotel[@"rooms"]) {
+                Room *newRoom = [NSEntityDescription insertNewObjectForEntityForName:@"Room" inManagedObjectContext:self.persistentContainer.viewContext];
+                
+                newRoom.roomNumber = (NSInteger)room[@"number"];
+                newRoom.beds = (NSInteger)room[@"beds"];
+                newRoom.rate = (NSInteger)room[@"rate"];
+                
+                newRoom.hotel = newHotel;
+            }
+        }
+    }
+    
+    NSError *saveError;
+    [self.persistentContainer.viewContext save:&saveError];
+    
+    if (saveError){
+        NSLog(@"Failed to save to Core Data.");
+    } else {
+        NSLog(@"Successfully saved to Core Data.");
+    }
+}
 
 #pragma mark - Core Data stack
 
