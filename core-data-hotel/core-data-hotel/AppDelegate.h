9271e2d9e397566a109437d42409f319309c0478
yanil3500 <yanil3500@gmail.com>
2017-04-24 09:31:22 -0700

    Adds project with Core Data enabled

---
+++ core-data-hotel/core-data-hotel/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  core-data-hotel
+//
+//  Created by Elyanil Liranzo Castro on 4/24/17.
+//  Copyright © 2017 Elyanil Liranzo Castro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
