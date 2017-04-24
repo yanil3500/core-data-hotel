143225d071e53d42a24455f82f79e11ba66eb272
yanil3500 <yanil3500@gmail.com>
2017-04-24 10:32:50 -0700

    adds auto-layout class;

---
+++ core-data-hotel/core-data-hotel/AutoLayout.m
@@ -0,0 +1,13 @@
+//
+//  AutoLayout.m
+//  core-data-hotel
+//
+//  Created by Elyanil Liranzo Castro on 4/24/17.
+//  Copyright © 2017 Elyanil Liranzo Castro. All rights reserved.
+//
+
+#import "AutoLayout.h"
+
+@implementation AutoLayout
+
+@end
